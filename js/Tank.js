667c83d853f0c1aad88f6f3c3caa2d90fad4544b
Pablo9298 <wmpawka@gmail.com>
2025-06-25 16:33:59 +0300

    PixiJs Tank

---
+++ js/Tank.js
@@ -0,0 +1,73 @@
+import { Container, AnimatedSprite, Texture, Sprite } from "./pixi.mjs";
+
+const createAnimatedSprite = (textureNames, position = {x: 0, y: 0}, anchor = {x: 0.5, y: 0.5}) => {
+    const texture = textureNames.map(name => Texture.from(name));
+
+    const animatedSprite = new AnimatedSprite(texture);
+    animatedSprite.position.copyFrom(position);
+    animatedSprite.anchor.copyFrom(anchor);
+    return animatedSprite;
+}
+
+const createSprite = (textureName, position = {x: 0, y: 0}, anchor = { x: 0.5, y: 0.5}) => {
+    const sprite = new Sprite(Texture.from(textureName));
+    sprite.position.copyFrom(position);
+    sprite.anchor.copyFrom(anchor);
+
+    return sprite;
+}
+
+export class Tank {
+    constructor() {
+        this._view = new Container();
+
+        this._bodyContainer = new Container();
+        this._view.addChild(this._bodyContainer);
+
+        this._tracksLeft = createAnimatedSprite(["TrackСFrame1", "TrackСFrame2"], {x: 0, y: -80});
+        this._tracksRight = createAnimatedSprite(["TrackСFrame1", "TrackСFrame2"], {x: 0, y: 80});
+        this._tracksLeft.animationSpeed = 0.25;
+        this._tracksRight.animationSpeed = 0.25; 
+
+        this._bodyContainer.addChild(this._tracksRight, this._tracksLeft);
+
+        this._hull = new Sprite(Texture.from("HeavyHullB"));
+        this._hull.anchor.set(0.5)
+
+        this._bodyContainer.addChild(this._hull);
+
+        this._towerContainer = new Container();
+        this._view.addChild(this._towerContainer);
+
+
+        this._towerContainer.addChild(createSprite("HeavyGunB", { x: 140, y: -27}))
+        this._towerContainer.addChild(createSprite("HeavyGunB", { x: 160, y: 29}))
+
+        this._towerContainer.addChild(createSprite("GunConnectorD", {x: 80, y: 0}))
+
+        this._towerContainer.addChild(createSprite("HeavyTowerB"))
+        
+    }
+
+    get view() {
+        return this._view;
+    }
+
+    rotateTowerBy(angle) {
+        this._towerContainer.rotation += angle;
+    }
+
+    rotateBodyBy(angle) {
+        this._bodyContainer.rotation += angle;
+    }
+
+    startTrack() {
+        this._tracksLeft.play();
+        this._tracksRight.play();
+    }
+
+    stopTrack() {
+        this._tracksLeft.stop();
+        this._tracksRight.stop();
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,30 @@
+import {Application, Graphics} from "./pixi.mjs";
+import { assetsMap } from "./assetsMap.js";
+import { Tank } from "./Tank.js";
+
+// Create the application
+const app = new Application({
+    width: 800,
+    height: 800,
+    backgroundColor: 0xc2c2c2,
+    view: document.getElementById("canvas"),
+});
+
+const runGame = () => {
+    const marker = new Graphics();
+    marker.beginFill(0xff0000, 1);
+    marker.drawCircle(0, 0 , 5);
+    marker.endFill();
+
+    const tank = new Tank();
+    app.stage.addChild(tank._view);
+    app.stage.addChild(marker);
+
+    app.stage.position.set(800 / 2, 800 / 2);
+
+    window["TANK"] = tank;
+
+};
+
+assetsMap.sprites.forEach((value) => app.loader.add(value.name, value.url));
+app.loader.load(runGame);
